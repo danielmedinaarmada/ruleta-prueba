8dfd75b858a1729e63af3e782a4302b75c650d2d
Daniel Medina <daniel@medinaarmada.com>
2019-10-15 19:04:05 -0400

    Finished 14

---
+++ calculate_prize.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+	
+  <? php
+    $d=mt_rand(1,8);
+    echo $d;
+  ?>
+
+</body>
+</html>
